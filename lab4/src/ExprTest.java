1dfdea5765ccebb90b6b1c3b4be66610e4ce81af
Dmitry Suvorov <dmsuvorov@devexperts.com>
2015-03-30 21:55:09 +0300

    test wrapper

--- lab4/src/ExprTest.java
+++ lab4/src/ExprTest.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,7 +9,22 @@ import java.text.ParseException;
  * Created by dmsuvorov on 10.03.2015.
  */
 public class ExprTest {
+	public static double eval(String input) throws ParseException {
+		InputStream is = new ByteArrayInputStream(input.getBytes());
+		ExprParser parser = new ExprParser();
+		return parser.parse(is);
+	}
+	
 	public static void main(String[] args) throws ParseException, IOException {
-		InputStream is = new ByteArrayInputStream("12".getBytes());
+		InputStreamReader converter = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(converter);
+
+		String curLine = "";
+		while (!(curLine.equals("quit"))) {
+			curLine = in.readLine();
+			if (!(curLine.equals("quit"))) {
+				System.out.println(eval(curLine));
+			}
+		}
 	}
 }
